c2ac39a8f53d5c4b172a68a9bbbca2489c92e8e4
Caleb Crouch <cecrouch01@gmail.com>
2023-08-15 18:33:38 -0400

    heroku bugs

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
-const api = require('./routes').app;
+const api = require('./routes/notes');
+
 
 const port = process.env.PORT || 3001
 
