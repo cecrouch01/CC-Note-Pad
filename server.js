e689441108e80c9e6dac21f563b7fcbdcdfc2eaa
Caleb Crouch <cecrouch01@gmail.com>
2023-08-15 15:51:39 -0400

    addition of comments to code

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = 3001;
 
 const app = express();
 
+//Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
@@ -17,11 +18,12 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-
+// GET Rout for Notes page
 app.get('/notes', (req, res) => (
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 ));
 
+// Server
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
